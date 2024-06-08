c7f784912ab103486509cb906709eb93acf4b073
shahid <wrteamshahid03@gmail.com>
2024-06-08 16:03:05 +0530

    modal added

--- script.js
+++ script.js
@@ -23,3 +23,36 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+
+  // Get the modal
+  var modal = document.getElementById("announcementModal");
+
+  // Get the span that closes the modal
+  var spanClose = document.getElementsByClassName("closeBtn")[0];
+
+  // Get all the spans with the rightArr class
+  var rightArrs = document.querySelectorAll(".rightArr");
+
+  // Loop through each rightArr span and add a click event listener
+  rightArrs.forEach(function(rightArr) {
+    rightArr.addEventListener("click", function() {
+      modal.style.display = "block";
+      document.body.classList.add("modal-open"); // Disable body scroll
+    });
+  });
+
+  // When the user clicks on <span> (x), close the modal
+  spanClose.onclick = function() {
+    modal.style.display = "none";
+    document.body.classList.remove("modal-open"); // Enable body scroll
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      document.body.classList.remove("modal-open"); // Enable body scroll
+    }
+  }
